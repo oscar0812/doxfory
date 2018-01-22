7cc5c5b6066b007b3cc98777b35bafc7b4fe4794
oscar0812 <oscar0812torres@gmail.com>
2018-01-21 23:28:08 -0600

    fixed register.php spacing

--- src/app/views/register.php
+++ src/app/views/register.php
@@ -1,196 +1,186 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
+<head>
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?=websiteName()?></title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>
+    <?=websiteName()?>
+  </title>
 
-        <!-- CSS -->
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/font-awesome.min.css">
-		    <link rel="stylesheet" href="css/form-elements.css">
-        <link rel="stylesheet" href="css/register.css">
-        <link type="text/css" rel="stylesheet" href="css/style.css" />
+  <!-- CSS -->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/form-elements.css">
+  <link rel="stylesheet" href="css/register.css">
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
 
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
 
-        <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="ico/favicon.png">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+  <!-- Favicon and touch icons -->
+  <link rel="shortcut icon" href="ico/favicon.png">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
 
-    </head>
+</head>
 
-    <body>
-      <!-- Nav -->
-  		<nav id="nav" class="navbar nav-transparent fixed-nav">
-  			<div class="container">
+<body>
+  <!-- Nav -->
+  <nav id="nav" class="navbar nav-transparent fixed-nav">
+    <div class="container">
 
-  				<div class="navbar-header">
-  					<!-- Logo -->
-  					<div class="navbar-brand">
-  						<a href="<?=$router->pathFor('home')?>">
+      <div class="navbar-header">
+        <!-- Logo -->
+        <div class="navbar-brand">
+          <a href="<?=$router->pathFor('home')?>">
   							<img class="logo" src="img/logo.png" alt="logo">
   							<img class="logo-alt" src="img/logo-alt.png" alt="logo">
   						</a>
-  					</div>
-  					<!-- /Logo -->
-  			</div>
-  		</nav>
-  		<!-- /Nav -->
-        <!-- Top content -->
-        <div class="top-content">
+        </div>
+        <!-- /Logo -->
+      </div>
+  </nav>
+  <!-- /Nav -->
+  <!-- Top content -->
+  <div class="top-content">
 
-            <div class="inner-bg">
-                <div class="container">
+    <div class="inner-bg">
+      <div class="container">
 
-                    <div class="row">
-                        <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1><strong>Login or Register</strong></h1>
-                            <div class="description">
-                            	<p>
+        <div class="row">
+          <div class="col-sm-8 col-sm-offset-2 text">
+            <h1><strong>Login or Register</strong></h1>
+            <div class="description">
+              <p>
 
-                            	</p>
-                            </div>
-                        </div>
-                    </div>
+              </p>
+            </div>
+          </div>
+        </div>
 
-                    <div class="row">
-                        <div class="col-sm-5">
+        <div class="row">
+          <div class="col-sm-5">
 
-                        	<div class="form-box">
-	                        	<div class="form-top">
-	                        		<div class="form-top-left">
-	                        			<h3>Login to our site</h3>
-	                            		<p>Enter username and password to log on:</p>
-	                        		</div>
-	                        		<div class="form-top-right">
-	                        			<i class="fa fa-key"></i>
-	                        		</div>
-	                            </div>
-	                            <div class="form-bottom">
-				                    <form role="form" action="" method="post" class="login-form">
-				                    	<div class="form-group">
-				                    		<label class="sr-only" for="form-email">Email</label>
-				                        	<input type="text" name="form-email" placeholder="Email..." class="form-email form-control">
-				                        </div>
-				                        <div class="form-group">
-				                        	<label class="sr-only" for="form-password">Password</label>
-				                        	<input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
-				                        </div>
-				                        <button type="submit" class="btn">Sign in!</button>
-				                    </form>
-			                    </div>
-		                    </div>
+            <div class="form-box">
+              <div class="form-top">
+                <div class="form-top-left">
+                  <h3>Login to our site</h3>
+                  <p>Enter username and password to log on:</p>
+                </div>
+                <div class="form-top-right">
+                  <i class="fa fa-key"></i>
+                </div>
+              </div>
+              <div class="form-bottom">
+                <form role="form" action="" method="post" class="login-form">
+                  <div class="form-group">
+                    <label class="sr-only" for="form-email">Email</label>
+                    <input type="text" name="form-email" placeholder="Email..." class="form-email form-control">
+                  </div>
+                  <div class="form-group">
+                    <label class="sr-only" for="form-password">Password</label>
+                    <input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
+                  </div>
+                  <button type="submit" class="btn">Sign in!</button>
+                </form>
+              </div>
+            </div>
 
-		                	<div class="social-login">
-	                        	<h3><strong>...or login with:</strong></h3>
-	                        	<div class="social-login-buttons">
-		                        	<a class="btn btn-link-1 btn-link-1-facebook" href="#">
-		                        		<i class="fa fa-facebook"></i> Facebook
-		                        	</a>
-		                        	<a class="btn btn-link-1 btn-link-1-twitter" href="#">
-		                        		<i class="fa fa-twitter"></i> Twitter
-		                        	</a>
-		                        	<a class="btn btn-link-1 btn-link-1-google-plus" href="#">
-		                        		<i class="fa fa-google-plus"></i> Google Plus
-		                        	</a>
-	                        	</div>
-	                        </div>
+          </div>
 
-                        </div>
+          <div class="col-sm-1 middle-border"></div>
+          <div class="col-sm-1"></div>
 
-                        <div class="col-sm-1 middle-border"></div>
-                        <div class="col-sm-1"></div>
+          <div class="col-sm-5">
 
-                        <div class="col-sm-5">
+            <div class="form-box">
+              <div class="form-top">
+                <div class="form-top-left">
+                  <h3>Sign up now</h3>
+                  <p>Fill in the form below to get instant access:</p>
+                </div>
+                <div class="form-top-right">
+                  <i class="fa fa-pencil"></i>
+                </div>
+              </div>
+              <div class="form-bottom">
+                <form role="form" action="" method="post" class="registration-form">
 
-                        	<div class="form-box">
-                        		<div class="form-top">
-	                        		<div class="form-top-left">
-	                        			<h3>Sign up now</h3>
-	                            		<p>Fill in the form below to get instant access:</p>
-	                        		</div>
-	                        		<div class="form-top-right">
-	                        			<i class="fa fa-pencil"></i>
-	                        		</div>
-	                            </div>
-	                            <div class="form-bottom">
-				                    <form role="form" action="" method="post" class="registration-form">
+                  <div class="form-group">
+                    <label class="sr-only" for="form-username">Username</label>
+                    <input type="text" name="form-username" placeholder="Username..." class="form-username form-control">
+                  </div>
+                  <div class="form-group">
+                    <label class="sr-only" for="form-email">Email</label>
+                    <input type="text" name="form-email" placeholder="Email..." class="form-email form-control">
+                  </div>
 
-                                <div class="form-group">
-				                        	<label class="sr-only" for="form-username">Username</label>
-				                        	<input type="text" name="form-username" placeholder="Username..." class="form-username form-control">
-				                        </div>
-				                        <div class="form-group">
-				                        	<label class="sr-only" for="form-email">Email</label>
-				                        	<input type="text" name="form-email" placeholder="Email..." class="form-email form-control">
-				                        </div>
+                  <div class="form-group">
+                    <label class="sr-only" for="form-pass">Password</label>
+                    <input type="text" name="form-pass" placeholder="Password..." class="form-pass form-control">
+                  </div>
 
-                                <div class="form-group">
-				                        	<label class="sr-only" for="form-pass">Password</label>
-				                        	<input type="text" name="form-pass" placeholder="Password..." class="form-pass form-control">
-				                        </div>
+                  <div class="form-group">
+                    <label class="sr-only" for="form-confirm-pass">Confirm Password</label>
+                    <input type="text" name="form-confirm-pass" placeholder="Confirm Password..." class="form-confirm-pass form-control">
+                  </div>
 
-                                <div class="form-group">
-				                        	<label class="sr-only" for="form-confirm-pass">Confirm Password</label>
-				                        	<input type="text" name="form-confirm-pass" placeholder="Confirm Password..." class="form-confirm-pass form-control">
-				                        </div>
-
-				                        <!--div class="form-group">
+                  <!--div class="form-group">
 				                        	<label class="sr-only" for="form-about-yourself">About yourself</label>
 				                        	<textarea name="form-about-yourself" placeholder="About yourself..."
 				                        				class="form-about-yourself form-control" id="form-about-yourself"></textarea>
 				                        </div-->
-				                        <button type="submit" class="btn">Sign me up!</button>
-				                    </form>
-			                    </div>
-                        	</div>
-
-                        </div>
-                    </div>
-
-                </div>
+                  <button type="submit" class="btn">Sign me up!</button>
+                </form>
+              </div>
             </div>
+
+          </div>
 
         </div>
 
-        <!-- Footer -->
-        <footer>
-        	<div class="container">
-        		<div class="row">
+      </div>
+    </div>
 
-        			<div class="col-sm-8 col-sm-offset-2">
-        				<div class="footer-border"></div>
-        				<p><?=copyright()?></p>
-        			</div>
+  </div>
 
-        		</div>
-        	</div>
-        </footer>
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row">
 
-        <!-- Javascript -->
-        <script src="js/main/jquery.min.js"></script>
-        <script src="js/main/jquery.backstretch.min.js"></script>
-        <script src="js/main/bootstrap.min.js"></script>
-        <script src="js/user/register.js"></script>
+        <div class="col-sm-8 col-sm-offset-2">
+          <div class="footer-border"></div>
+          <p>
+            <?=copyright()?>
+          </p>
+        </div>
 
-        <!--[if lt IE 10]>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Javascript -->
+  <script src="js/main/jquery.min.js"></script>
+  <script src="js/main/jquery.backstretch.min.js"></script>
+  <script src="js/main/bootstrap.min.js"></script>
+  <script src="js/user/register.js"></script>
+
+  <!--[if lt IE 10]>
             <script src="js/placeholder.js"></script>
         <![endif]-->
 
-    </body>
+</body>
 
 </html>
