34feadd65449c84a6256ea4879b88a3c18e4b8b3
oscar0812 <oscar0812torres@gmail.com>
2018-01-25 12:43:01 -0600

    same navbar template for /user/ and /

--- src/app/views/templates/navbar.php
+++ src/app/views/templates/navbar.php
@@ -1,15 +1,20 @@
-<?if(!$logged_in) { ?>
 <!-- Nav -->
-<nav id="nav" class="navbar nav-transparent">
+<nav id="nav" class="navbar <?if(!$logged_in) echo "nav-transparent"; else echo"fixed-nav"?>">
   <div class="container">
 
     <div class="navbar-header">
       <!-- Logo -->
       <div class="navbar-brand">
+        <?if(!$logged_in) {?>
         <a href="<?=$home?>">
           <img class="logo" src="<?=$home?>img/logo.png" alt="logo">
           <img class="logo-alt" src="<?=$home?>img/logo-alt.png" alt="logo">
         </a>
+        <? } else { ?>
+          <a href="<?=$router->pathFor('profile')?>">
+            <h2><?=$current_user->getUsername()?></h2>
+          </a>
+        <? } ?>
       </div>
       <!-- /Logo -->
 
@@ -22,6 +27,7 @@
 
     <!--  Main navigation  -->
     <ul class="main-nav nav navbar-nav navbar-right">
+      <?if(!$logged_in) {?>
       <li><a href="#home">Home</a></li>
       <li><a href="#about">About</a></li>
       <li class="has-dropdown"><a href="#featured">Jobs</a>
@@ -31,53 +37,13 @@
         </ul>
       </li>
       <li><a href="#contact">Contact</a></li>
+      <? } else {?>
+      <li><a href="<?=$router->pathFor('job')?>">Jobs</a></li>
+      <li><a href="<?=$router->pathFor('signout')?>" id="signout">Sign Out</a></li>
+      <? } ?>
     </ul>
     <!-- /Main navigation -->
 
   </div>
 </nav>
 <!-- /Nav -->
-
-<? } else { ?>
-  <!-- Nav -->
-  <nav id="nav" class="navbar">
-    <div class="container">
-
-      <div class="navbar-header">
-        <!-- Logo -->
-        <div class="navbar-brand">
-          <a href="<?=$home?>">
-            <img class="logo" src="<?=$home?>img/logo.png" alt="logo">
-          </a>
-        </div>
-        <!-- /Logo -->
-
-        <!-- Collapse nav button -->
-        <div class="nav-collapse">
-          <span></span>
-        </div>
-        <!-- /Collapse nav button -->
-      </div>
-
-      <!--  Main navigation  -->
-      <ul class="main-nav nav navbar-nav navbar-right">
-        <li><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#">6</a></li>
-        <li class="has-dropdown"><a>7</a>
-          <ul class="dropdown">
-            <li><a href="#">8</a></li>
-          </ul>
-        </li>
-        <li><a href="<?=$router->pathFor('signout')?>" id="signout">Sign Out</a></li>
-      </ul>
-      <!-- /Main navigation -->
-
-    </div>
-  </nav>
-  <!-- /Nav -->
-
-<? } ?>
