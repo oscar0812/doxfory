a1f096edcd0002b4e4c096826f7905a55c2b8d69
oscar0812 <oscar0812torres@gmail.com>
2018-01-24 23:27:38 -0600

    moved navbar to template folder

--- src/app/views/home.php
+++ src/app/views/home.php
@@ -48,44 +48,7 @@
 		</div>
 		<!-- /Background Image -->
 
-		<!-- Nav -->
-		<nav id="nav" class="navbar nav-transparent">
-			<div class="container">
-
-				<div class="navbar-header">
-					<!-- Logo -->
-					<div class="navbar-brand">
-						<a href="<?=$home?>">
-							<img class="logo" src="<?=$home?>img/logo.png" alt="logo">
-							<img class="logo-alt" src="<?=$home?>img/logo-alt.png" alt="logo">
-						</a>
-					</div>
-					<!-- /Logo -->
-
-					<!-- Collapse nav button -->
-					<div class="nav-collapse">
-						<span></span>
-					</div>
-					<!-- /Collapse nav button -->
-				</div>
-
-				<!--  Main navigation  -->
-				<ul class="main-nav nav navbar-nav navbar-right">
-					<li><a href="#home">Home</a></li>
-					<li><a href="#about">About</a></li>
-					<li class="has-dropdown"><a href="#featured">Jobs</a>
-						<ul class="dropdown">
-							<li><a href="#featured">Featured</a></li>
-							<li><a href="#recent">Recent</a></li>
-						</ul>
-					</li>
-					<li><a href="#contact">Contact</a></li>
-				</ul>
-				<!-- /Main navigation -->
-
-			</div>
-		</nav>
-		<!-- /Nav -->
+		<?php require_once('templates/navbar.php') ?>
 
 		<!-- home wrapper -->
 		<div class="home-wrapper">
