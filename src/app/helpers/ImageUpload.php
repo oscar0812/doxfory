28a239e06c5cd7da4db84b06c564054fc216901c
oscar0812 <oscar0812torres@gmail.com>
2018-01-25 22:04:57 -0600

    successfully uploaded image files

--- src/app/helpers/ImageUpload.php
+++ src/app/helpers/ImageUpload.php
@@ -3,48 +3,56 @@ namespace App\Helpers;
 
 class ImageUpload
 {
-    public static function upload($target_dir)
+    private static $root_dir = 'uploads/';
+
+    public static function createDir($target_dir)
     {
+        if (!file_exists($target_dir)) {
+            mkdir($target_dir);
+        }
+    }
+
+    public static function uploadPfp($id)
+    {
+        $dir = ImageUpload::$root_dir;
+        ImageUpload::createDir($dir);
+        $dir .='pfp/';
+        ImageUpload::createDir($dir);
+        return ImageUpload::upload($dir, $id);
+    }
+    
+    public static function upload($dir, $id)
+    {
+        $target_dir = $dir;
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
         // Check if image file is a actual image or fake image
         if (isset($_POST["submit"])) {
             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if ($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $uploadOk = 1;
-            } else {
-                echo "File is not an image.";
-                $uploadOk = 0;
+            if ($check == false) {
+                return ['success'=>false, 'msg'=>'File is not an image'];
             }
         }
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            echo "Sorry, file already exists.";
-            $uploadOk = 0;
-        }
+
         // Check file size
         if ($_FILES["fileToUpload"]["size"] > 500000) {
-            echo "Sorry, your file is too large.";
-            $uploadOk = 0;
+            return ['success'=>false, 'msg'=>'File is too large'];
         }
         // Allow certain file formats
         if ($imageFileType != "jpg" && $imageFileType != "png" &&
         $imageFileType != "jpeg" && $imageFileType != "gif") {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $uploadOk = 0;
+            return ['success'=>false, 'msg'=>'Only JPG, JPEG, PNG & GIF files are allowed'];
         }
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-            // if everything is ok, try to upload file
+
+        // change image name to ($user->id).extension
+        $ext = substr($target_file, strripos($target_file, '.'));
+        $target_file = $dir.$id.$ext;
+
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            //echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            return ['success'=>true, 'msg'=>'File uploaded', 'path'=>$target_file];
         } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
+            return ['success'=>false, 'msg'=>'There was an error uploading your file'];
         }
     }
 }
