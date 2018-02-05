efd6fdfd70a8711d0425482f32050f36c7de9146
oscar0812 <oscar0812torres@gmail.com>
2018-02-05 09:54:25 -0600

    spacing

--- data/generated-conf/config.php
+++ data/generated-conf/config.php
@@ -3,20 +3,20 @@ $serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
 $serviceContainer->checkVersion('2.0.0-dev');
 $serviceContainer->setAdapterClass('default', 'mysql');
 $manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
-$manager->setConfiguration(array (
+$manager->setConfiguration(array(
   'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=doxfory',
   'user' => 'root',
   'password' => '',
   'settings' =>
-  array (
+  array(
     'charset' => 'utf8',
     'queries' =>
-    array (
+    array(
     ),
   ),
   'classname' => '\\Propel\\Runtime\\Connection\\ConnectionWrapper',
   'model_paths' =>
-  array (
+  array(
     0 => 'src',
     1 => 'vendor',
   ),
