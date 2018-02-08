fed8c71494b1ebc73e2689a885988788090a92b7
oscar0812 <oscar0812torres@gmail.com>
2018-02-08 13:16:24 -0600

    create_job view updated

--- app/views/create_job.php
+++ app/views/create_job.php
@@ -1,9 +1,138 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <p>Create job</p>
-  </body>
+<!DOCTYPE html>
+<?php $home = $router->pathFor('home')?>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title><?=websiteName()?></title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
+
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:700,400|Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
+
+	<!-- Bootstrap -->
+	<link type="text/css" rel="stylesheet" href="<?=$home?>css/main/bootstrap.min.css" />
+
+	<!-- Owl Carousel -->
+	<link type="text/css" rel="stylesheet" href="<?=$home?>css/main/owl.carousel.css" />
+	<link type="text/css" rel="stylesheet" href="<?=$home?>css/main/owl.theme.default.css" />
+
+	<!-- Magnific Popup -->
+	<link type="text/css" rel="stylesheet" href="<?=$home?>css/main/magnific-popup.css" />
+
+	<!-- Font Awesome Icon -->
+	<link rel="stylesheet" href="<?=$home?>css/main/font-awesome.min.css">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="<?=$home?>css/user/agency.css" />
+	<link type="text/css" rel="stylesheet" href="<?=$home?>css/user/profile.css" />
+
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+
+	<!-- Header -->
+	<header>
+
+		<?php require_once('templates/navbar.php') ?>
+
+	</header>
+	<!-- /Header -->
+
+  <!-- Form -->
+  <div id="contact" class="section md-padding">
+
+    <!-- Container -->
+    <div class="container">
+
+      <!-- Row -->
+      <div class="row">
+
+        <!-- Section-header -->
+        <div class="section-header text-center">
+          <h2 class="title">Post a new job</h2>
+        </div>
+        <!-- /Section-header -->
+
+        <!-- info form -->
+        <div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1">
+          <form class="contact-form">
+            <div class="row">
+              <div>
+                <input type="text" class="input" placeholder="Title">
+
+                <label>
+                  <button type="button" class="btn btn-primary" name="button">Upload image</button>
+                </label>
+              </div>
+
+            </div>
+            <div class="row">
+              <textarea class="input" placeholder="Job Description"></textarea>
+            </div>
+
+
+            <div class="row">
+              <button class="main-btn">Post Job</button>
+            </div>
+          </form>
+        </div>
+        <!-- /info form -->
+
+      </div>
+      <!-- /Row -->
+
+    </div>
+    <!-- /Container -->
+
+  </div>
+  <!-- /Form -->
+
+
+	<!-- Footer -->
+	<footer>
+		<div class="container">
+			<div class="row">
+
+				<div class="col-sm-12">
+					<p class="text-center">
+						<?=copyright()?>
+					</p>
+				</div>
+
+			</div>
+		</div>
+	</footer>
+	<!-- /Footer -->
+
+	<!-- Back to top -->
+	<div id="back-to-top"></div>
+	<!-- /Back to top -->
+
+
+	<!-- jQuery Plugins -->
+	<script type="text/javascript" src="<?=$home?>js/main/jquery.min.js"></script>
+	<script type="text/javascript" src="<?=$home?>js/main/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?=$home?>js/main/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="<?=$home?>js/main/jquery.magnific-popup.js"></script>
+	<script type="text/javascript" src="<?=$home?>js/user/agency.js"></script>
+	<script type="text/javascript" src="<?=$home?>js/user/profile.js"></script>
+
+	</script>
+
+</body>
+
 </html>
