249789679028eece45a5eb7b249a07b201df95eb
oscar0812 <oscar0812torres@gmail.com>
2018-02-15 15:06:34 -0600

    fix click when changing div to main

--- html/js/user/jobs.js
+++ html/js/user/jobs.js
@@ -1,8 +1,9 @@
 $(function() {
 
-  $('#main_div').on('click', '.row', function() {
-    // redirect to specific job 
+  $('main').on('click', '.row', function() {
+    // redirect to specific job
     url = $(this).attr('data-url');
     window.location.href = url;
   });
+
 });
