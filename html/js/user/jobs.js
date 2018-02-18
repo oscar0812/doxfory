8346dc8684a4c395715d5d685bf3fed052a17914
oscar0812 <oscar0812torres@gmail.com>
2018-02-17 19:31:08 -0600

    remove unused jobs js code

--- html/js/user/jobs.js
+++ html/js/user/jobs.js
@@ -1,26 +1,3 @@
 $(function() {
-
-  $('main').on('click', '.agency-box', function() {
-    // redirect to specific job
-    url = $(this).attr('data-url');
-    window.location.href = url;
-  });
-
   search($('#search'), $('#main>.row'), ['.job-title', '.job-description']);
-
-  /*
-  $('#search').on('input', function() {
-    search_text = $(this).val();
-    // search all the rows
-    $('#main>.row').each(function() {
-      title = $(this).find('.job-title').eq(0).text();
-      description = $(this).find('.job-description').eq(0).text();
-      if (contains(title, search_text) || contains(description, search_text)) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    });
-  });
-  */
 });
