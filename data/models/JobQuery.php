3535674ef684790573b41ad4c00ea190430c714c
oscar0812 <oscar0812torres@gmail.com>
2018-02-24 14:03:43 -0600

    custom sql query to get jobs by proximity to latitude and longitude

--- data/models/JobQuery.php
+++ data/models/JobQuery.php
@@ -2,6 +2,10 @@
 
 use Base\JobQuery as BaseJobQuery;
 
+use Propel\Runtime\Propel;
+use Propel\Runtime\Formatter\ObjectFormatter;
+use Map\JobTableMap;
+
 /**
  * Skeleton subclass for performing query and update operations on the 'job' table.
  *
@@ -14,21 +18,38 @@ use Base\JobQuery as BaseJobQuery;
  */
  use Propel\Runtime\ActiveQuery\Criteria;
 
+ class JobQuery extends BaseJobQuery
+ {
+     public function completed()
+     {
+         return $this->filterByIsCompleted('yes');
+     }
 
-class JobQuery extends BaseJobQuery
-{
-    public function completed()
-    {
-        return $this->filterByIsCompleted('yes');
-    }
+     public function notCompleted()
+     {
+         return $this->filterByIsCompleted('no');
+     }
 
-    public function notCompleted()
-    {
-        return $this->filterByIsCompleted('no');
-    }
+     public function newestToOldest()
+     {
+         return $this->orderByTimePosted(Criteria::DESC);
+     }
 
-    public function newestToOldest()
-    {
-        return $this->orderByTimePosted(Criteria::DESC);
-    }
-}
+     public static function orderByProximity($lat, $lon)
+     {
+         $sf = M_PI / 180; // scaling factor
+
+         $con = Propel::getWriteConnection(\Map\JobTableMap::DATABASE_NAME);
+         // dist = acos[ sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lng1-lng2) ]
+         $sql = "SELECT * FROM job
+         ORDER BY ACOS(SIN(latitude*$sf)*SIN($lat*$sf) + COS(latitude*$sf)*COS($lat*$sf)*COS((longitude-$lon)*$sf))";
+
+         $stmt = $con->prepare($sql);
+         $stmt->execute();
+
+         $formatter = new ObjectFormatter();
+         $formatter->setClass('\Job'); //full qualified class name
+         $jobs = $formatter->format($con->getDataFetcher($stmt));
+         return $jobs;
+     }
+ }
