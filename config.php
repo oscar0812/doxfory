58315978cd62a7c55c50a403c822fdd8de5149d0
oscar0812 <oscar0812torres@gmail.com>
2018-02-24 15:51:42 -0600

    function to modify url params, ex: query=all => query=not_all

--- config.php
+++ config.php
@@ -71,6 +71,35 @@ function endsWith($original, $substr)
     (substr($original, -$length) === $substr);
 }
 
+function modifyUrl($url = '', $mod = array())
+{
+    // If $url wasn't passed in, use the current url
+    if ($url == false) {
+        $scheme = $_SERVER['SERVER_PORT'] == 80 ? 'http' : 'https';
+        $url = $scheme.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    }
+
+    // Parse the url into pieces
+    $url_array = parse_url($url);
+
+    // The original URL had a query string, modify it.
+    if (!empty($url_array['query'])) {
+        parse_str($url_array['query'], $query_array);
+        foreach ($mod as $key => $value) {
+            if (!empty($query_array[$key])) {
+                $query_array[$key] = $value;
+            }
+        }
+    }
+
+    // The original URL didn't have a query string, add it.
+    else {
+        $query_array = $mod;
+    }
+
+    return $url.'?'.http_build_query($query_array);
+}
+
 // date functions
 function getCurrentDateTime()
 {
