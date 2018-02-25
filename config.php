3085fd15ae180d1d45e198a9f351b9feccf635e7
oscar0812 <oscar0812torres@gmail.com>
2018-02-25 12:55:26 -0600

    fixed bugs with modifyUrl

--- config.php
+++ config.php
@@ -71,12 +71,11 @@ function endsWith($original, $substr)
     (substr($original, -$length) === $substr);
 }
 
-function modifyUrl($url = '', $mod = array())
+function modifyUrl($mod = array(), $url = false)
 {
     // If $url wasn't passed in, use the current url
     if ($url == false) {
-        $scheme = $_SERVER['SERVER_PORT'] == 80 ? 'http' : 'https';
-        $url = $scheme.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        $url = url();
     }
 
     // Parse the url into pieces
@@ -86,9 +85,7 @@ function modifyUrl($url = '', $mod = array())
     if (!empty($url_array['query'])) {
         parse_str($url_array['query'], $query_array);
         foreach ($mod as $key => $value) {
-            if (!empty($query_array[$key])) {
-                $query_array[$key] = $value;
-            }
+            $query_array[$key] = $value;
         }
     }
 
@@ -97,7 +94,7 @@ function modifyUrl($url = '', $mod = array())
         $query_array = $mod;
     }
 
-    return $url.'?'.http_build_query($query_array);
+    return $url_array['scheme'].'://'.$url_array['host'].$url_array['path'].'?'.http_build_query($query_array);
 }
 
 // date functions
